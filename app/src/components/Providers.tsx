63fccccfcb80a9f9c7151c6d89d18b6155d1c730
Chii Yuen <chii.yuen@hotmail.com>
2025-11-07 13:45:02 +0800

    Add providers component

---
+++ app/src/components/Providers.tsx
@@ -0,0 +1,31 @@
+'use client';
+
+import { wrappedFetch } from '@/lib/api';
+import { ReactNode } from 'react';
+import { SWRConfig } from 'swr';
+import { SettingsProvider } from '@/providers/SettingsProvider';
+import { ProgramProvider } from '@/providers/ProgramProvider';
+import { SolanaProvider } from '@/providers/SolanaProvider';
+import { TableProvider } from '@/providers/TableProvider';
+
+export function Providers({ children }: { children: ReactNode }) {
+  return (
+    <SWRConfig
+      value={{
+        suspense: false,
+        revalidateOnFocus: false,
+        fetcher: wrappedFetch,
+      }}
+    >
+      <SettingsProvider>
+        <SolanaProvider>
+          <ProgramProvider>
+            <TableProvider>
+              {children}
+            </TableProvider>
+          </ProgramProvider>
+        </SolanaProvider>
+      </SettingsProvider>
+    </SWRConfig>
+  );
+}
