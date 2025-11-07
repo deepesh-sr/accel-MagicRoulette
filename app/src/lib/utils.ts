112b1bc58c2787ad38a79388524b7e59782ee7cb
Chii Yuen <chii.yuen@hotmail.com>
2025-11-08 00:50:54 +0800

    Fix @solana-developers/helpers bug
    For some what reason getExplorerLink only runs in a Node.js environment

--- app/src/lib/utils.ts
+++ app/src/lib/utils.ts
@@ -1,4 +1,4 @@
-import { VersionedTransaction } from "@solana/web3.js";
+import { Cluster, VersionedTransaction } from "@solana/web3.js";
 
 export function v0TxToBase64(tx: VersionedTransaction): string {
   return Buffer.from(tx.serialize()).toString('base64');
@@ -7,3 +7,39 @@ export function v0TxToBase64(tx: VersionedTransaction): string {
 export function boolToByte(value: boolean): string {
   return value ? '1' : '0';
 }
+
+// https://github.com/solana-developers/helpers/blob/main/src/lib/explorer.ts#L20
+const encodeURL = (baseUrl: string, searchParams: Record<string, string>) => {
+  const url = new URL(baseUrl);
+  url.search = new URLSearchParams(searchParams).toString();
+  return url.toString();
+};
+
+// https://github.com/solana-developers/helpers/blob/main/src/lib/explorer.ts#L30
+export const getExplorerLink = (
+  linkType: "transaction" | "tx" | "address" | "block",
+  id: string,
+  cluster: Cluster | "localnet" = "mainnet-beta",
+): string => {
+  const searchParams: Record<string, string> = {};
+  if (cluster !== "mainnet-beta") {
+    if (cluster === "localnet") {
+      // localnet technically isn't a cluster, so requires special handling
+      searchParams["cluster"] = "custom";
+      searchParams["customUrl"] = "http://localhost:8899";
+    } else {
+      searchParams["cluster"] = cluster;
+    }
+  }
+  let baseUrl: string = "";
+  if (linkType === "address") {
+    baseUrl = `https://explorer.solana.com/address/${id}`;
+  }
+  if (linkType === "transaction" || linkType === "tx") {
+    baseUrl = `https://explorer.solana.com/tx/${id}`;
+  }
+  if (linkType === "block") {
+    baseUrl = `https://explorer.solana.com/block/${id}`;
+  }
+  return encodeURL(baseUrl, searchParams);
+};
