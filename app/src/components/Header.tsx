ebb2f3b6632587d182fcb3ebb7e52e1d9aa2d0c5
Chii Yuen <chii.yuen@hotmail.com>
2025-11-08 12:03:25 +0800

    Add Header

---
+++ app/src/components/Header.tsx
@@ -0,0 +1,17 @@
+"use client";
+
+import { UnifiedWalletButton } from "@jup-ag/wallet-adapter";
+import { LifeBuoy } from "lucide-react";
+import Link from "next/link";
+
+export function Header() {
+  return (
+    <header className="flex w-full items-center justify-between p-4 bg-primary h-20">
+      <Link className="flex gap-2 items-center" href={"/"}>
+        <LifeBuoy />
+        <h1 className="font- text-2xl">Magic Roulette</h1>
+      </Link>
+      <UnifiedWalletButton />
+    </header>
+  )
+}
