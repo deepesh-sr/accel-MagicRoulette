32896fd9a0b0a9ccdb610952dc1cd375a6ccfc2c
Chii Yuen <chii.yuen@hotmail.com>
2025-11-10 02:01:47 +0800

    Remove DEFAULT_QUEUE constant

--- app/src/lib/constants.ts
+++ app/src/lib/constants.ts
@@ -1,8 +1,1 @@
-import { PublicKey } from "@solana/web3.js";
-
 export const DISCRIMINATOR_SIZE = 8;
-
-// https://docs.rs/ephemeral-vrf-sdk/latest/src/ephemeral_vrf_sdk/consts.rs.html#8
-export const DEFAULT_QUEUE = new PublicKey(
-  "Cuj97ggrhhidhbu39TijNVqE74xvKJ69gDervRUXAxGh"
-);
